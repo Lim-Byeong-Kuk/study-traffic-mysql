9ab188ed78bbf73d8f53d9f41641bea002acb364
Lim-Byeong-Kuk <bye0414@naver.com>
2024-03-21 18:43:34 +0900

    페이지네이션 구현
    order by 절 뒤에 들어갈 쿼리를 구현해주는 PageHelper
    를 이용하여 페이지네이션을 구현 하였다.

---
+++ src/main/java/com/example/fastcampusmysql/domain/PageHelper.java
@@ -0,0 +1,24 @@
+package com.example.fastcampusmysql.domain;
+
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public class PageHelper {
+
+    /**
+     *  order by 뒤에 들어갈 sql 쿼리를 만들어주는 역할
+     */
+    public static String orderBy(Sort sort) {
+        if (sort.isEmpty()) {
+            return "id DESC";
+        }
+
+        List<Sort.Order> orders = sort.toList();
+        List<String> orderBys = orders.stream()
+                .map(order -> order.getProperty() + " " + order.getDirection())
+                .toList();
+
+        return String.join(", ", orderBys);
+    }
+}
--- src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/fastcampusmysql/domain/post/repository/PostRepository.java
@@ -1,11 +1,16 @@
 package com.example.fastcampusmysql.domain.post.repository;
 
+import com.example.fastcampusmysql.domain.PageHelper;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
 import com.example.fastcampusmysql.domain.post.entity.Post;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -13,6 +18,7 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -22,7 +28,15 @@ public class PostRepository {
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    static final RowMapper<DailyPostCount> DAILY_POST_COUNT_MAPPER = (ResultSet resultSet, int rowNum)
+    static final private RowMapper<Post> ROW_MAPPER = (ResultSet resultSet, int rowNum) -> Post.builder()
+            .id(resultSet.getLong("id"))
+            .memberId(resultSet.getLong("memberId"))
+            .contents(resultSet.getString("contents"))
+            .createdDate(resultSet.getObject("createdDate", LocalDate.class))
+            .createdAt(resultSet.getObject("createdAt", LocalDateTime.class))
+            .build();
+
+    static final private RowMapper<DailyPostCount> DAILY_POST_COUNT_MAPPER = (ResultSet resultSet, int rowNum)
             -> new DailyPostCount(
                     resultSet.getLong("memberId"),
                     resultSet.getObject("createdDate", LocalDate.class),
@@ -39,6 +53,36 @@ public class PostRepository {
 
         var params = new BeanPropertySqlParameterSource(request);
         return namedParameterJdbcTemplate.query(sql, params, DAILY_POST_COUNT_MAPPER);
+    }
+
+    public Page<Post> findAllByMemberId(Long memberId, Pageable pageable) {
+        var params = new MapSqlParameterSource()
+                .addValue("memberId", memberId)
+                .addValue("size", pageable.getPageSize())
+                .addValue("offset", pageable.getOffset());
+
+        var sql = String.format("""
+                SELECT *
+                FROM %s
+                WHERE memberId = :memberId
+                ORDER BY %s
+                LIMIT :size
+                OFFSET :offset
+                """, TABLE, PageHelper.orderBy(pageable.getSort()));
+
+        List<Post> posts = namedParameterJdbcTemplate.query(sql, params, ROW_MAPPER);
+        return new PageImpl(posts, pageable, getCount(memberId));
+
+    }
+
+    private Long getCount(Long memberId) {
+        var sql = String.format("""
+                SELECT count(id)
+                FROM %s
+                WHERE memberId = :memberId
+                """, TABLE);
+        var params = new MapSqlParameterSource().addValue("memberId", memberId);
+        return namedParameterJdbcTemplate.queryForObject(sql, params, Long.class);
     }
 
     public Post save(Post post) {
